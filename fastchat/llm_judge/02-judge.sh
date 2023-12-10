ee485ea44102f79925f753df6173108c95369049
lhl <lhl@randomfoo.net>
2023-12-10 15:54:13 +0000

    testing llm-jp-13b

--- fastchat/llm_judge/02-judge.sh
+++ fastchat/llm_judge/02-judge.sh
@@ -1,4 +1,6 @@
 # time python gen_judgment.py --bench-name japanese_mt_bench --model-list shisa-bad-7b-v1 --judge-file data/judge_ja_prompts.jsonl --parallel 2
 # time python gen_judgment.py --bench-name japanese_mt_bench --model-list shisa-gamma-7b-v1 --judge-file data/judge_ja_prompts.jsonl --parallel 2
 # time python gen_judgment.py --bench-name japanese_mt_bench --model-list shisa-mega-dpo-7b-v1.1 --judge-file data/judge_ja_prompts.jsonl --parallel 2
-time python gen_judgment.py --bench-name japanese_mt_bench --model-list shisa-mega-7b-v1.2-dpo --judge-file data/judge_ja_prompts.jsonl --parallel 2
+# time python gen_judgment.py --bench-name japanese_mt_bench --model-list shisa-mega-7b-v1.2-dpo --judge-file data/judge_ja_prompts.jsonl --parallel 2
+time python gen_judgment.py --bench-name japanese_mt_bench --model-list llm-jp-13b-instruct-full-jaster-dolly-oasst-v1.0 --judge-file data/judge_ja_prompts.jsonl --parallel 2
+# time python gen_judgment.py --bench-name japanese_mt_bench --model-list llm-jp-13b-instruct-full-jaster-v1.0 --judge-file data/judge_ja_prompts.jsonl --parallel 2
