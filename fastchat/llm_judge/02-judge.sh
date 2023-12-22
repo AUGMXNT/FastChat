534e0770f467115fd966f6a073274f07520f4f86
lhl <lhl@randomfoo.net>
2023-12-23 05:48:30 +0900

    latest models

--- fastchat/llm_judge/02-judge.sh
+++ fastchat/llm_judge/02-judge.sh
@@ -12,4 +12,6 @@
 # time python gen_judgment.py --bench-name japanese_mt_bench --model-list shisa-7B-v1-GPTQ --judge-file data/judge_ja_prompts.jsonl --parallel 2
 # time python gen_judgment.py --bench-name japanese_mt_bench --model-list shisa-7B-v1-AWQ --judge-file data/judge_ja_prompts.jsonl --parallel 2
 # time python gen_judgment.py --bench-name japanese_mt_bench --model-list Swallow-70b-instruct-GPTQ --judge-file data/judge_ja_prompts.jsonl --parallel 2
-time python gen_judgment.py --bench-name japanese_mt_bench --model-list Qwen-72b-Chat --judge-file data/judge_ja_prompts.jsonl --parallel 2
+# time python gen_judgment.py --bench-name japanese_mt_bench --model-list Qwen-72B-Chat --judge-file data/judge_ja_prompts.jsonl --parallel 2
+# time python gen_judgment.py --bench-name japanese_mt_bench --model-list Xwin-LM-70B-V0.1-GPTQ --judge-file data/judge_ja_prompts.jsonl --parallel 2
+time python gen_judgment.py --bench-name japanese_mt_bench --model-list nekomata-14b-instruction --judge-file data/judge_ja_prompts.jsonl --parallel 2
