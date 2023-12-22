5b2ea7fd3796a60721a0864498ed27494a626983
lhl <lhl@randomfoo.net>
2023-12-22 16:08:41 +0900

    latest tests

--- fastchat/llm_judge/02-judge.sh
+++ fastchat/llm_judge/02-judge.sh
@@ -6,5 +6,10 @@
 # time python gen_judgment.py --bench-name japanese_mt_bench --model-list llm-jp-13b-instruct-full-jaster-v1.0 --judge-file data/judge_ja_prompts.jsonl --parallel 2
 # time python gen_judgment.py --bench-name japanese_mt_bench --model-list houou-instruction-7b-v1 --judge-file data/judge_ja_prompts.jsonl --parallel 2
 # time python gen_judgment.py --bench-name japanese_mt_bench --model-list houou-instruction-7b-v1-correctedtemplate2 --judge-file data/judge_ja_prompts.jsonl --parallel 2
-time python gen_judgment.py --bench-name japanese_mt_bench --model-list tokyotech-llm_Swallow-7b-instruct-hf --judge-file data/judge_ja_prompts.jsonl --parallel 2
+# time python gen_judgment.py --bench-name japanese_mt_bench --model-list tokyotech-llm_Swallow-7b-instruct-hf --judge-file data/judge_ja_prompts.jsonl --parallel 2
 # time python gen_judgment.py --bench-name japanese_mt_bench --model-list shisa-7b-v1-fullprompt --judge-file data/judge_ja_prompts.jsonl --parallel 2
+# time python gen_judgment.py --bench-name japanese_mt_bench --model-list tokyotech-llm_Swallow-13b-instruct-hf --judge-file data/judge_ja_prompts.jsonl --parallel 2
+# time python gen_judgment.py --bench-name japanese_mt_bench --model-list shisa-7B-v1-GPTQ --judge-file data/judge_ja_prompts.jsonl --parallel 2
+# time python gen_judgment.py --bench-name japanese_mt_bench --model-list shisa-7B-v1-AWQ --judge-file data/judge_ja_prompts.jsonl --parallel 2
+# time python gen_judgment.py --bench-name japanese_mt_bench --model-list Swallow-70b-instruct-GPTQ --judge-file data/judge_ja_prompts.jsonl --parallel 2
+time python gen_judgment.py --bench-name japanese_mt_bench --model-list Qwen-72b-Chat --judge-file data/judge_ja_prompts.jsonl --parallel 2
